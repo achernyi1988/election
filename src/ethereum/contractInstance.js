065200ac236a541ddcce5c9f45b13d68b62194a0
achernyi <ote1188@gmail.com>
2019-03-30 14:32:28 +0200

    Done

--- src/ethereum/contractInstance.js
+++ src/ethereum/contractInstance.js
@@ -2,7 +2,7 @@ import web3 from "../ethereum/web3"
 const compiledFactory = require("../ethereum/build/Election");
 
 // PRIVATE_KEY = "109D58463D2A21022382C21C9A4FA0CDDAA6E20B4FDF9CFD2304E182DCC56CBE";
-const CONTRACT_ADDRESS = "0xfBF6e0Ff3F33F958fa71Ed25F5a2C9F207Cdf5a7";  //rinkby
+const CONTRACT_ADDRESS = "0xb9258f83C6c3339Dab568D356A77dd22c6B00042";  //rinkby
 //const CONTRACT_ADDRESS = "0x5fa4FbcE6b2896FaF6d2157853d5501441a969e5";    //ganache
 
 
