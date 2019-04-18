77b5a2058d4e1732ca276436f235cca1cdcd6492
achernyi <ote1188@gmail.com>
2019-04-18 22:31:34 +0300

    Move ethereum api to server

--- server.js
+++ server.js
@@ -1,14 +1,72 @@
+
+const proxy = require("./server/web3Proxy");
+const bodyParser = require('body-parser');
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = process.env.PORT || 9000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.use(express.static(path.join(__dirname, 'build')));
+//
+// app.get('/*', function(req, res) {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/getIPFSHash', (req, res) => {
+    console.log("getIPFSHash" );
+    proxy.getIPFSHash(function (data) {
+        res.send(data);
+    });
 });
 
-app.listen(9000,(err) =>{
+app.post('/setIPFSHash', (req, res) => {
+    console.log("setIPFSHash" );
+    proxy.setIPFSHash(req.body.hash, function (data) {
+        res.send(data);
+    });
+});
+
+
+app.get('/getContractAddress', (req, res) => {
+    console.log("getContractAddress" );
+    proxy.getContractAddress(function (data) {
+        res.send(data);
+    });
+});
+
+app.get('/getElectorateVoted', (req, res) => {
+    console.log("getElectorateVoted" );
+    proxy.getElectorateVoted(function (data) {
+        res.send(data);
+    });
+});
+
+app.get('/getCandidates', (req, res) => {
+    console.log("getCandidates" );
+    proxy.getCandidates(function (data) {
+        res.send(data);
+    });
+});
+
+app.get('/getElectionResult', (req, res) => {
+    console.log("getElectionResult" );
+    proxy.getElectionResult(function (data) {
+        res.send(data);
+    });
+});
+
+app.post('/vote', (req, res) => {
+    console.log("vote", req.body.candidate, req.body.electorate );
+    proxy.vote(req.body.candidate, req.body.electorate, function (data) {
+        res.send(data);
+    });
+});
+
+app.listen(port,(err) =>{
     if(err)  throw err;
-    console.log("Ready on localhost:9000");
+    console.log(`Ready on localhost:${port}`);
 });
---
+++ server/ethereum/contracts/Election.sol
@@ -0,0 +1,142 @@
+pragma solidity ^0.4.25;
+
+contract IElection{
+    function vote(string  _contender, string _electorate) external ;
+    function registerContender(string  _fullName, string  _description) external;
+    function getNoOfVoted(string  _fullName) external view returns (uint256);
+    function getWinner() external view returns (string memory, string memory, uint256);
+}
+
+contract Election is IElection{
+
+    address public admin;
+
+    struct ContenderData{
+        string fullName;
+        string description;
+        uint256 voteCounter;
+    }
+
+    struct VoterData{
+        string voter;
+        string contender;
+    }
+
+    ContenderData [] public contender;
+    mapping(string => uint256) contenderId;
+    uint256 public currentContenderID;
+    mapping(string => bool) contenderRegistered;
+    mapping(string => bool) electorateVoted;
+
+    mapping(string => bool) votersMap;
+    VoterData [] public votersArray;
+
+    constructor()public {
+        admin = msg.sender;
+
+        addContender("Зеленский В.А.", "Democratia" );
+        addContender("Порошенко П.А. ", "Slyga Narody" );
+        addContender("Тимошенко Ю.В.", "Partia svoboda" );
+
+        addContender("Бойко Ю.А.", "Vosrojdenie" );
+        addContender("Гриценко А.С.", "Vosrojdenie" );
+        addContender("Ляшко О.В.", "nacialistu" );
+        addContender("Смешко И.П.", "Vosrojdenie" );
+        addContender("Вилкул А.Ю.", "Vosrojdenie" );
+
+        addContender("Садовый А.И.", "Vosrojdenie" );
+
+        addContender("Против всех", "Vosrojdenie" );
+    }
+
+    string ipfsHash;
+
+
+    event OnIPFSHash(string hash);
+    event OnVote(string contender, string electorate);
+
+    function getNumberOfVoter() public view returns (uint){
+        return votersArray.length;
+    }
+    function getNumberOfContender() public view returns (uint){
+        return contender.length;
+    }
+
+
+    function setIPFS(string hash)  public onlyAdmin {
+        ipfsHash = hash;
+        emit OnIPFSHash (ipfsHash);
+    }
+
+    function getIPFS()  public view returns (string){
+        return ipfsHash;
+    }
+
+    function registerContender(string  _fullName, string  _description) external onlyAdmin{
+        addContender(_fullName, _description);
+    }
+
+    function addContender(string memory _fullName, string memory _description) private {
+        require(contenderRegistered[_fullName] == false, "a contender already added ");
+
+        contenderRegistered[_fullName] = true;
+
+        contenderId[_fullName] = ++currentContenderID;
+
+        contender.push(ContenderData({fullName:_fullName, description: _description, voteCounter:0}));
+
+    }
+
+    function vote(string  _contender, string _electorate ) external  NotContender(_contender) {
+
+        require(!electorateVoted[_electorate], "this electorate has already voted");
+
+        ContenderData storage voted = contender[getContenderIndex(_contender)];
+
+        voted.voteCounter++;
+
+        electorateVoted[_electorate] = true;
+
+        votersArray.push(VoterData(_electorate , _contender));
+
+        emit OnVote(_contender, _electorate);
+    }
+
+    function getNoOfVoted(string  _fullName) external view NotContender(_fullName)  returns (uint256){
+        return contender[getContenderIndex(_fullName)].voteCounter;
+    }
+
+
+    function getWinner() external view returns (string memory,string memory, uint256){
+        require(0 < contender.length, "0 < contender.length");
+        uint256 max = 0;
+        ContenderData memory winner = contender[0];
+        for(uint256 i = 0; i < contender.length; i++){
+            if(contender[i].voteCounter > max){
+                max = contender[i].voteCounter;
+                winner = contender[i];
+            }
+        }
+        return(winner.fullName, winner.description, winner.voteCounter);
+    }
+
+    function getContenderIndex(string memory _fullName) private view returns(uint256){
+        require(contenderId[_fullName]  > 0, "not valid contender ID");
+        return contenderId[_fullName] - 1;
+    }
+
+    modifier notAdmin(){
+        require(msg.sender != admin, "admin cannot vote");
+        _;
+    }
+
+    modifier onlyAdmin(){
+        require(msg.sender == admin, "only admin");
+        _;
+    }
+
+    modifier NotContender(string memory _fullName){
+        require(contenderRegistered[_fullName], "a contender is not available ");
+        _;
+    }
+}
